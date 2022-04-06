e83332321243bac4d36ffb0e76ea92d536f6c6dc
glinux99 <interlab21@yahoo.com>
2022-04-06 17:20:34 +0200

    L

---
+++ i_menu/menug-md.php
@@ -0,0 +1,39 @@
+<div>
+<ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
+        <li class="nav-item text-center">
+            <a href="" class="nav-link">
+                <i class="bi--2xl bi-globe"></i> <span class="ms-0"><?php echo _("Actualite");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-chat-right-dots"></i> <span class="ms-0"><?php echo _("Humours");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-briefcase"></i> <span class="ms-0"><?php echo _("Services");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-person"></i> <span class="ms-0"><?php echo _("Incription");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-file-earmark-pdf"></i> <span class="ms-0"><?php echo _("Livres");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-folder2-open"></i> <span class="ms-0"><?php echo _("Tutoriels");?></span>
+            </a>
+        </li>
+        <li class="nav-item text-center">
+            <a href="" class="nav-link h-50">
+                <i class="bi--2xl bi-music-note-list"></i> <span class="ms-0"><?php echo _("Songs");?></span>
+            </a>
+        </li>  
+    </ul>
+</div>
--- index.php
+++ index.php
@@ -4,55 +4,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include('i_menu/header.php');?>
-    <link rel="stylesheet" href="css/css.css">
     <title>Document</title>
 </head>
 <body>
     <?php include('i_menu/menu_p.php');?>
     <div class="body">
-        <div class="bg-success row mx-1">
-            <div class="row col-3" >
-                <div style="position: absolute; z-index: 1;" class="m-0 p-0">
-                    <button class="btn float-start position-fixed" >
-                        <i class="bi bi-list-columns bi--3xl arrounded text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#menug" aria-controls="menug"></i>
+        <div class="bg-success row mx-1 position-relative">
+            <div class="col-3 p-0">
+                <div class="title bg-danger px-2 d-flex align-items-center " style="clip-path: polygon(0 0, 100% 0%, 70% 100%, 0% 100%); border: 1px solid green; height: 7vh;">
+                    Urgence
+                </div>
+                <div style="position: absolute; z-index: 1;" class="d-md-none d-lg-none d-block">
+                    <button class="btn float-start position-fixed m-0 p-0">
+                        <i class="bi-list-columns bi--3xl text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#menug" aria-controls="menug"></i>
                     </button>
                 </div>
-            </div>
+            </div>           
             <div class="message col-9">
                 Une bombe a explose dans un quartier de la ville mais n'a pas fait beaucoup de degat
             </div>
         </div>
-        <div id="body_div">
+        <div class="d-md-flex d-lg-flex">
+            <div class="d-md-block  d-lg-block d-none">
+                <?php include('i_menu/menug-md.php');?>
+            </div>
+            <div id="body_div" class="d-md-none  d-lg-none d-block">
                 <!-- Menu gauche flottant -->
                 <div class="menug">
                     <?php include('i_menu/menug.php');?>
                 </div>
                 <!-- Fin du boutton menu gauche flottant et son button -->
-        </div>
-        <div class="group-3">   
-            <?php while ($x<6){?> 
-                <div class="card position-relative text-white">
-                    <img src="i_img/inter7.jpg" alt="image" class="card-img-top" height="350">
-                    <div class="card-img-overlay m-0 p-0">
-                        <div class="card-title bg-dark p-2 h-25 overflow-hidden">
-                            <div style="clip-path: polygon(0 0, 100% 0%, 70% 100%, 0% 100%);" class="bg-danger py-1 ps-2">Securite</div>
-                            <div>
-                            Attaques des ADF a masisi Attaques des ADF a masisi
-                            Attaques des ADF a masisi Attaques des ADF a masisi Attaques des ADF a masisi Attaques des ADF a masisi
-                            Attaques des ADF a masisi Attaques des ADF a masisi
+            </div>
+            <div>
+                <div class="group-3">
+                    <?php while($x<6){?>
+                        <div class="card mx-1 text-white">
+                            <img src="i_img/inter7.jpg" alt="image" class="card-img-top" height="350">
+                            <div class="card-img-overlay m-0 p-0">
+                                <div class="card-title bg-dark p-2 h-25 overflow-hidden">
+                                    <div style="clip-path: polygon(0 0, 100% 0%, 70% 100%, 0% 100%);" class="bg-danger py-1 ps-2">Securite</div>
+                                    <div>
+                                        Attaques des ADF a masisi Attaques des ADF a masisi
+                                    Attaques des ADF a masisi Attaques des ADF a masisi Attaques des ADF a masisi Attaques des ADF a masisi
+                                    Attaques des ADF a masisi Attaques des ADF a masisi
+                                    </div>
+                                </div>
+                                <div class="card-body fixed-bottom position-absolute px-2" style="max-height: 40%;overflow: hidden;">
+                                    Je suis un des survivants mais on a juste eu la chance
+                                    Je suis un des survivants mais on a juste eu la chance
+                                    Je suis un des survivants mais on a juste eu la chance
+                                    Je suis un des survivants mais on a juste eu la chance
+                                    Je suis un des survivants mais on a juste eu la chance
+                                    Je suis un des survivants mais on a juste eu la chance
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body fixed-bottom position-absolute px-2" style="max-height: 40%;overflow: hidden;">
-                            Je suis un des survivants mais on a juste eu la chance
-                            Je suis un des survivants mais on a juste eu la chance
-                            Je suis un des survivants mais on a juste eu la chance
-                            Je suis un des survivants mais on a juste eu la chance
-                            Je suis un des survivants mais on a juste eu la chance
-                            Je suis un des survivants mais on a juste eu la chance
-                        </div>
-                    </div>
+                    <?php $x++;}?>
                 </div>
-            <?php $x++;}?>
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-dark px-3 me-1">Back</button>
+                    <button class="btn btn-success px-3">Next</button>
+                </div>
+            </div>
         </div>
     </div>
     <div>
