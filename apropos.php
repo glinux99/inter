cf121666c3edb8eb67c3165df2fc0d0b9b6e4248
glinux99 <interlab21@yahoo.com>
2022-04-06 23:01:02 +0200

    L

--- apropos.php
+++ apropos.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <?php include('i_menu/menu_p.php');?>
-    <div class="d-md-flex d-lg-flex">
+    <div class="d-md-flex d-lg-flex body">
         <div class="menu-g">
             <div class="d-md-block  d-lg-block d-none">
                 <?php include('i_menu/menug-md.php');?>
@@ -24,8 +24,13 @@
         </div>
         <div class="vrai-body parallax-window w-100" data-parallax="scroll" data-image-src="i_img/inter7.jpg">
             <!-- Code ici -->
-            <div class="card-title text-center text-white">
-                <h1>A PROPOS</h1>
+            <div class="card-title text-center text-center h1 text-white">
+                <span class="soulign">A PROPOS</span>
+            </div>
+            <div class="text-black mx-1">
+                Pour ce qui concernent Interlab, Interlab est une societe mis au point par des informaticiens 
+                pour aider d'aitres utilisateurs sur des problemes a caractere informatique. ce site web a ete mis au
+                point par la premiere graduat des Ingenieurs en Informatique de l'ISTA/GOMA de l'annee 2018-2019:
             </div>
         </div>
     </div>
