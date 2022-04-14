666c589d8c63f3215ff3f033566a91779552fc39
linux99 <linux99@localhost.localdomain>
2022-04-14 15:30:31 +0200

    L

--- tutoriels/tutos.php
+++ tutoriels/tutos.php
@@ -21,26 +21,38 @@
                 <!-- Fin du boutton menu gauche flottant et son button -->
             </div>
         </div>
-        <div class="">
+        <div class="my-3">
             <!-- Code ici -->
-            <div class="border-start border-2 border-success ps-2">
-                <h4>Tous les livres</h4>
+            <div class="row p-0 m-0 px-1">
+                <div class="col-md-6 border-start border-2 border-success ps-2">
+                    <h4>Tous les tutoriels en videos et pdf</h4>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end">
+                        <input type="search" name="" id="" class="form-control" style="width: 25vh;">
+                        <button class="btn btn-success">Rechercher</button>
+                </div>
             </div>
             <div class="row w-100 mx-auto">
                 <?php $livres=[2,3,4,5,5,5,5,4];
             foreach($livres as $livre){
+                $play ="bi-play-btn-fill";
+                $play ="bi-file-earmark-pdf-fill";
                 echo '<div class="card col-6 col-md-3">
+                        <div class="card-img-overlay text-center d-flex pb-5" id="vid1">
+                            <button class="btn align-self-center mx-auto " >
+                                <span class="'.$play.' bi--4xl text-danger"></span>
+                            </button>
+                        </div>
                     <img src="../i_pdf/img1.jpg" alt="" class="card-img-top" height="200" width="400">
                     <div class="card-body p-0">
-                        <span class="">
-                            Titre:
-                        </span>
-                        <span class="">
-                            Autheur:
-                        </span>
+                        <h3 class="text-center">MySql livre tuto </h3>
                     </div>
                 </div>';
             }?>
+            </div>
+            <div class="d-flex justify-content-center my-2">
+                <button class="btn btn-dark px-3 me-1">Back</button>
+                <button class="btn btn-success px-3">Next</button>
             </div>
         </div>
     </div>
