df6d8da7ad22f5eca811835771622af5330bc077
linux99 <linux99@localhost.localdomain>
2022-04-15 17:20:22 +0200

    inter

--- i_songs/songs.php
+++ i_songs/songs.php
@@ -7,12 +7,35 @@
     <link rel="stylesheet" href="player/css/main.css">
     <title>Document</title>
 </head>
-<style>
- 
-</style>
 <body>
-  <div class="col-md-4 player-one" style="max-width: 25rem">    
-  </div>
-<script src="player/js/main.js"></script>
+    <?php include($dir.'i_menu/menu_p.php');?>
+    <div class="d-md-flex d-lg-flex">
+        <div class="menu-g col-1">
+            <div class="d-md-block  d-lg-block d-none">
+                <?php include($dir.'i_menu/menug-md.php');?>
+            </div>
+            <div id="body_div" class="d-md-none  d-lg-none d-block">
+                <!-- Menu gauche flottant -->
+                <div class="menug">
+                    <?php include($dir.'i_menu/menug.php');?>
+                </div>
+                <!-- Fin du boutton menu gauche flottant et son button -->
+            </div>
+        </div>
+        <div class="vw-100">
+            <!-- Code ici -->
+            <div class="border-start border-2 border-success ps-2">
+                <h4>Tous les musiques</h4>
+            </div>
+            <div class="row w-100 mx-auto">
+                  <div class="col-md-4 player-one" style="max-width: 25rem"></div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <?php include($dir.'i_menu/footer.php');?>
+    </div>
+    <script src="player/js/main.js"></script>
 </body>
 </html>
+
