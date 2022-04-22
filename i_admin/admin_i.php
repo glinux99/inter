46952033bb3aae60273499cb94ba652d1ed27bf0
glinux99 <interlab21@yahoo.com>
2022-04-22 13:12:10 +0200

    inter

--- i_admin/admin_i.php
+++ i_admin/admin_i.php
@@ -41,46 +41,76 @@
                 <h4 class="text-center text-uppercase">Administrateur de site</h4>
             </div>
             <div class="col-md-9 mx-auto my-2 text-center"> 
-                <div class="row mx-1 ">
+                <div class="row mx-1 " id="article_humour">
                     <div class="card shadow col-6" style="height: 23ex;" onclick="article();">
-                        <div class="card-header">
-                            Articles
-                        </div>
-                        <div class="card-body text-center">
-                            <span class="bi-chat-square-text bi--8xl"></span>
-                        </div>
+                        <a href="#articles">
+                            <div class="card-header">
+                                Articles
+                            </div>
+                            <div class="card-body text-center">
+                                <span class="bi-chat-square-text bi--8xl"></span>
+                            </div>
+                        </a>
                     </div>
                     <div class="card shadow col-6" style="height: 23ex;" onclick="humour();">
+                        <a href="#humours">
+                            <div class="card-header">
+                                Humours
+                            </div>
+                            <div class="card-body text-center">
+                                <span class="bi-emoji-heart-eyes-fill bi--8xl"></span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="" data-bs-spy="scroll" data-bs-target="#article_humour" data-bs-offset="0" tabindex="0">
+                    <div id="articles" style="display: none;" class="card shadow">
                         <div class="card-header">
-                            Humours
+                            Editer/Publier un article
                         </div>
-                        <div class="card-body text-center">
-                            <span class="bi-emoji-heart-eyes-fill bi--8xl"></span>
-                        </div>
-                    </div>
-                </div>
-                <div id="articles" style="display: none;" class="card shadow">
-                    <div class="card-header">
-                        Editer/Publier un article
-                    </div>
-                    <div class="card-body px-1">
-                        <button class="btn btn-success">Ajouter <span class="bi-plus-circle"></span></button>
-                        <button class="btn btn-success">Modifier <span class="bi-pencil-square"></span></button>
-                        <button class="btn btn-danger">Supprimer <span class="bi-trash"></span></button>
-                        <div class="article text-start">
-                            <h4 class="text-start border-start border-success border-3 ps-3">Liste des articles rescents</h4>
-                            <ul class="article-body mx-4 list-unstyled">
-                                <?php while($x<5){
-                                    echo '<li class="list-unstyled-item">
-                                        <a href="" class="nav-link p-0 m-0">Bombardement en Ukraine</a></li>';
-                                    $x++;
-                                }?>
-                            </ul>
+                        <div class="card-body px-1">
+                            <button class="btn btn-success">Ajouter <span class="bi-plus-circle"></span></button>
+                            <button class="btn btn-success">Modifier <span class="bi-pencil-square"></span></button>
+                            <button class="btn btn-danger">Supprimer <span class="bi-trash"></span></button>
+                            <div class="article text-start">
+                                <h4 class="text-start border-start border-success border-3 ps-3">Liste des articles rescents</h4>
+                                <ul class="article-body mx-4 list-unstyled">
+                                    <?php while($x<5){
+                                        echo '<li class="list-unstyled-item">
+                                            <a href="" class="nav-link p-0 m-0">Bombardement en Ukraine</a></li>';
+                                        $x++;
+                                    }?>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div id="humours" style="display: none;" class="card shadow">
-                    Humours
+                    <div id="humours" style="display: none;" class="card shadow">
+                        <div class="card-header">
+                            Faites sourire quelqu'un
+                        </div>
+                        <div class="card-body">
+                            <div class="card shadow m-2 p-2">
+                                <div class="input-group">
+                                    <label for="">Selection Ribrique</label>
+                                    <div class="mb-3 ms-2">
+                                      <select class="form-select" aria-label="Default select example">
+                                        <option value="" disabled>Messages</option>
+                                        <option value="1" selected>Sms amour</option>
+                                        <option value="2">Sms blague</option>
+                                        <option value="3">Proverbes</option>
+                                        <option value="">Citations</option>
+                                        <option value="">Autres</option>
+                                        <option value="" disabled>Video/Photo</option>
+                                        <option value="">Photo</option>
+                                        <option value="">Video</option>
+                                        <option value="">Gift</option>
+                                        <option value="">Autres</option>
+                                      </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="row mx-1 ">
                     <div class="card shadow col-6" style="height: 23ex;">
