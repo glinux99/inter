ed1e740d27e91e4f5e6c423496f5fa3a5f4578b7
glinux99 <interlab21@yahoo.com>
2022-05-05 12:57:25 +0200

    inter

--- ess2.php
+++ ess2.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include('i_menu/header.php');?>
-    <title>Document</title>
-</head>
-<body>
-    <?php echo $_POST['titre'];?>
-    <?php echo $_POST['description'];?>
-</body>
-</html>
+
+<?php
+    $pub ='<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>'.$_POST["titre"].'</title>
+    </head>
+    <body>
+        '. $_POST['titre'].'
+        <div class="container">
+            '. $_POST['description'].'
+        </div>
+    </body>
+    </html>';
+    $file ="ess3.php";
+    $fopen =(fopen($file,'w'));
+    fwrite($fopen, $pub);
+    fclose($fopen);
